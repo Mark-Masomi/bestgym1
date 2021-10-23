2621fa5cf3b80f8f2bead2f60172a6adee5a305c
Moin93 <Moinmirmasomi@outlook.com>
2021-10-23 21:20:41 +0200

    new class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        gymkaos gymkaos=new gymkaos();
+        PT_login_file pt_login_file=new PT_login_file("Alhambra Aromes");
     }
 }
--- src/com/company/kund.java
+++ src/com/company/kund.java
@@ -3,18 +3,13 @@ package com.company;
 import java.time.LocalDate;
 
 public class kund {
-    @Override
-    public String toString() {
-        return "kund{" +
-                "name='" + name + '\'' +
-                ", personNum='" + personNum + '\'' +
-                ", bet_av_årsavgift=" + bet_av_årsavgift +
-                '}';
-    }
-
+    public final static String filePath = "src/com/company/customers.txt";
+    public final static String inloggning_PT="src/com/company/loginfile.txt";
+    public final static LocalDate valid_date=LocalDate.now().minusYears(1);
     private String name;
     private String personNum;
     private LocalDate bet_av_årsavgift;
+
 
 
     public String getName() {
@@ -31,9 +26,21 @@ public class kund {
 
 
 
+
+
     public kund(String name, String personNum, LocalDate bet_av_årsavgift) {
         this.name = name;
         this.personNum = personNum;
         this.bet_av_årsavgift = bet_av_årsavgift;
+    }
+
+
+    @Override
+    public String toString() {
+        return "kund{" +
+                "name='" + name + '\'' +
+                ", personNum='" + personNum + '\'' +
+                ", bet_av_årsavgift=" + bet_av_årsavgift +
+                '}';
     }
 }
